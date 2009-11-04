984226762770d301ab64a4d328282f3e05cc7df4
root <root@domU-12-31-39-04-F2-75.compute-1.internal>
2009-11-04 15:43:35 +0000

    Dammit alec, i should have to.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 ActionController::Routing::Routes.draw do |map|
   map.resources :categories
 
-  map.resources :sections, :has_many => :categoriesxf
+  map.resources :sections, :has_many => :categories
 
   # The priority is based upon order of creation: first created -> highest priority.
 
