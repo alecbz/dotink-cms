a8e381299832e0d478eea78395ae2696ff7bfde8
Alec Benzer <alecbenzer@gmail.com>
2009-11-04 10:41:32 -0500

    added new routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 ActionController::Routing::Routes.draw do |map|
   map.resources :categories
 
-  map.resources :sections
+  map.resources :sections, :has_many => :categoriesxf
 
   # The priority is based upon order of creation: first created -> highest priority.
 
