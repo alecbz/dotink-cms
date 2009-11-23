ab57d34eb735bc4f4696cf3945287ba9fd936fe5
= <phunny.phacts@gmail.com>
2009-11-23 18:43:06 -0500

    active scaffold dependency

--- config/environment.rb
+++ config/environment.rb
@@ -22,6 +22,7 @@ Rails::Initializer.run do |config|
   # config.gem "rubyist-aasm", :lib => "aasm", :source => "http://gem.github.com"
   config.gem 'acts_as_ferret'
   config.gem 'will_paginate', :version => '~> 2.3.11', :source => 'http://gemcutter.org'
+  config.gem 'activescaffold'
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
   # config.plugins = [ :exception_notification, :ssl_requirement, :all ]
