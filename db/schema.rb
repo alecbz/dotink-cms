966b3f4548f04323a7f5f99dffbf51951e7a14c0
= <phunny.phacts@gmail.com>
2009-11-23 19:09:47 -0500

    implemented typus admin panel

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20091118145541) do
+ActiveRecord::Schema.define(:version => 20091124000737) do
 
   create_table "articles", :force => true do |t|
     t.string   "title"
@@ -29,6 +29,20 @@ ActiveRecord::Schema.define(:version => 20091118145541) do
 
   create_table "sections", :force => true do |t|
     t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "typus_users", :force => true do |t|
+    t.string   "first_name",       :default => "",    :null => false
+    t.string   "last_name",        :default => "",    :null => false
+    t.string   "role",                                :null => false
+    t.string   "email",                               :null => false
+    t.boolean  "status",           :default => false
+    t.string   "token",                               :null => false
+    t.string   "salt",                                :null => false
+    t.string   "crypted_password",                    :null => false
+    t.string   "preferences"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
