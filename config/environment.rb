5d0a33f55f374d88409d81a0bfa35ec7093adffe
Mark Fayngersh <mark@markserv.(none)>
2010-01-27 09:57:35 -0500

    fixed a bunch of plugin dependencies

--- config/environment.rb
+++ config/environment.rb
@@ -20,8 +20,6 @@ Rails::Initializer.run do |config|
   # config.gem "sqlite3-ruby", :lib => "sqlite3"
   # config.gem "aws-s3", :lib => "aws/s3"
   # config.gem "rubyist-aasm", :lib => "aasm", :source => "http://gem.github.com"
-  config.gem 'acts_as_ferret'
-  config.gem 'will_paginate', :version => '~> 2.3.11', :source => 'http://gemcutter.org'
   # config.gem 'typus', :source => 'http://gemmcutter.org'
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
